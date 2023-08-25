41c2757177eaf81e64b95346aa37302542bd7ed7
Marc Plogas <marc.plogas@digitalnatives.de>
2023-08-26 00:41:49 +0200

    added old effect as a starting point

--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,8 @@ class RingLayout
 {
 protected:
     //ring0 and ring_all are required!
-    const uint16_t Rings[5] = {0, 16, 32, 44, PixelCount}; 
-    
+    const uint16_t Rings[4] = {0, 16, 16 + 32, PixelCount};
+
     uint8_t _ringCount() const
     {
         return sizeof(Rings) / sizeof(Rings[0]);
@@ -36,6 +36,17 @@ void setup()
     Serial.printf("rings available: ");
     Serial.print(topo.getCountOfRings());
     Serial.println();
+    for (uint16_t r = 0; r < topo.getCountOfRings(); r++) {
+            Serial.println();
+            Serial.printf("ring: ");
+            Serial.print(r);
+            Serial.printf(" start: ");
+            Serial.print(topo.Map(r, 0));
+            Serial.printf(" end: ");
+            Serial.print(topo.Map(r, topo.getPixelCountAtRing(r) - 1));
+            Serial.println();
+    }
+
     Serial.printf("total pixels available: ");
     Serial.print(topo.getPixelCount());
 
@@ -48,51 +59,72 @@ void setup()
 
 void loop()
 {
-    // // strip testcode
-    // Serial.println();
-    // Serial.println("blink 5th and 10th pixel ...");
-
-    // testStrip.SetPixelColor(5,red);
-    // testStrip.SetPixelColor(10,red);
-    // testStrip.Show();
-    // delay(2500);
-    // testStrip.SetPixelColor(5,green);
-    // testStrip.SetPixelColor(10,green);
-    // testStrip.Show();
-    // delay(2500);
-    // testStrip.SetPixelColor(5,blue);
-    // testStrip.SetPixelColor(10,blue);
-    // testStrip.Show();
-    // delay(2500);
-    
     Serial.println();
     Serial.println("entering loop");
-    delay(2500);    
 
     // use the topo to map the 2d polar cordinate to the pixel
     // and use that to SetPixelColor
-    for (uint16_t r = 0; r < topo.getCountOfRings(); r++)
+    for (uint16_t r = topo.getCountOfRings(); r > 0; r--)
     {
         Serial.println();
         Serial.printf("setting ring ");
-        Serial.print(r);
+        Serial.print(r - 1);
         Serial.println();
-        for(uint16_t p = 0; p < topo.getPixelCountAtRing(r); p++)
+
+        // BUG :(
+        //set previous ring black
+        if(r - 1 < topo.getCountOfRings() - 1) {
+            for(uint16_t p = 0; p < topo.getPixelCountAtRing(r); p++)
+            {
+                Serial.printf("blacking pixel ");
+                Serial.print(p);
+                Serial.printf(" overall pixel ");
+                Serial.print(topo.Map(r, p));
+                Serial.println();
+                strip.SetPixelColor(topo.Map(r, p), black);
+
+            }
+        }
+        strip.Show();
+
+        //set actual ring color
+        for(uint16_t p = 0; p < topo.getPixelCountAtRing(r - 1); p++)
         {
             Serial.printf("setting pixel ");
             Serial.print(p);
             Serial.printf(" overall pixel ");
-            Serial.print(topo.Map(r, p));
+            Serial.print(topo.Map(r - 1, p));
             Serial.println();
-            strip.SetPixelColor(topo.Map(r, p), red);
+            strip.SetPixelColor(topo.Map(r - 1, p), red);
+
         }
+        strip.Show();
+        delay(500);
     }
-    strip.Show();
-    delay(5000);    
+
 
     Serial.println();
     Serial.println("Clearing to black");
     strip.ClearTo(black);
     strip.Show();
     delay(1000);
+}
+
+void testStrip() {
+    // strip testcode
+    Serial.println();
+    Serial.println("blink 5th and 10th pixel ...");
+
+    strip.SetPixelColor(5,red);
+    strip.SetPixelColor(10,red);
+    strip.Show();
+    delay(2500);
+    strip.SetPixelColor(5,green);
+    strip.SetPixelColor(10,green);
+    strip.Show();
+    delay(2500);
+    strip.SetPixelColor(5,blue);
+    strip.SetPixelColor(10,blue);
+    strip.Show();
+    delay(2500);
 }
